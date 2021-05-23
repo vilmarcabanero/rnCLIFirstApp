03a01eb93d4c32373bfb15088b8d5e7119c739a4
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-05-24 00:41:28 +0800

    RN CLI version of G app, hello G! first commit

---
+++ App.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import {Button, View, Text, StyleSheet} from 'react-native';
+
+const App = () => {
+  const [outputText, setOutputText] = useState('Hello G!');
+
+  const ask = () => {
+    setOutputText('Can I?');
+  };
+
+  return (
+    <>
+      <View style={styles.container}>
+        <Text style={styles.text}>{outputText}</Text>
+        <Button title="Can I ask you something?" onPress={ask} />
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+  },
+  text: {
+    color: 'steelblue',
+    marginBottom: 5,
+    fontSize: 16,
+  },
+});
+
+export default App;
